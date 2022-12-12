b829c4cb1f5f56fd8f8de26b487e373455816e9d
Fireflasher <4oli.brose@gmail.com>
2022-12-12 23:45:56 +0100

    Change 1.18.2 4.2 (Unversioned)

--- src/main/java/fireflasher/forgerplog/mixins/PauseScreenMixin.java
+++ src/main/java/fireflasher/forgerplog/mixins/PauseScreenMixin.java
@@ -1,32 +1,31 @@
 package fireflasher.forgerplog.mixins;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import fireflasher.forgerplog.config.modmenu.Optionsscreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.screens.ChatOptionsScreen;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.logging.Logger;
+
 @Mixin(PauseScreen.class)
-public abstract class GuiButtonMixin{
+public abstract class PauseScreenMixin extends Screen {
 
-    @Shadow protected abstract void lambda$createPauseMenu$0(Button par1);
-
-    @Inject(method = "init", at = @At("HEAD"),cancellable = true)
-    private void init(CallbackInfo ci){
-        Button accessModOption = new Button(0, 0, 10, 10, Component.nullToEmpty("RPL"), button -> {
-            Minecraft.getInstance().setScreen(new Optionsscreen());
-        });
-        this.lambda$createPauseMenu$0(accessModOption);
-
-
+    protected PauseScreenMixin(Component p_96550_) {
+        super(p_96550_);
     }
 
+    @Inject(method = ("createPauseMenu"), at = @At("HEAD"))
+    public void createPauseMenu(CallbackInfo callbackInfo){
+        Button accessModOption = new Button(0, 0, 35, 20, Component.nullToEmpty("RPL"), button -> {
+            Minecraft.getInstance().setScreen(new Optionsscreen(this));
+        });
+        addRenderableWidget(accessModOption);
+        Logger.getLogger("Debug").info("ModButton");
+    }
 }
