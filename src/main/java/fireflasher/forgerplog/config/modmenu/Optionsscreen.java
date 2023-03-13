b50626dd7f71db83094391c5b65f2792f662d1d4
Fireflasher21 <4oli.brose@gmail.com>
2023-03-13 16:12:32 +0100

    Added Meta Data

--- src/main/java/fireflasher/forgerplog/config/modmenu/Optionsscreen.java
+++ src/main/java/fireflasher/forgerplog/config/modmenu/Optionsscreen.java
@@ -67,7 +67,7 @@ public class Optionsscreen extends Screen {
 
         Button addServer = new Button(this.width / 2 - this.width / 4 - 50, 13, 100, BUTTON_HEIGHT, Component.translatable("rplog.config.optionscreen.add_Server"),
                 button ->{
-                    if (Minecraft.getInstance().getCurrentServer() != null || !Minecraft.getInstance().getCurrentServer().isLan()) {
+                    if (Minecraft.getInstance().getCurrentServer() != null && !Minecraft.getInstance().getCurrentServer().isLan()) {
                         String[] ip = new String[2];
                         ip[0] = Minecraft.getInstance().getCurrentServer().ip;
                         ip[1] = Minecraft.getInstance().getCurrentServer().name;
